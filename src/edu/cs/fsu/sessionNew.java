9d1dbb2fa1962fa0d50f245e8dda3f879b9d8b46
John Nguyen <nguyen.john@me.com>
2012-04-19 18:27:48 -0400

    Add password field when creating a new session. The field will be used to rejoin the master view

--- src/edu/cs/fsu/sessionNew.java
+++ src/edu/cs/fsu/sessionNew.java
@@ -1,6 +1,8 @@
 package edu.cs.fsu;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
 
 import org.apache.http.client.ClientProtocolException;
 
@@ -17,7 +19,7 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 public class sessionNew extends Activity {
-	String fname, lname, sessName, sID;
+	String fname, lname, sessName, sID, sessionPassword;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -30,7 +32,7 @@ public class sessionNew extends Activity {
 		s.setAdapter(adapter);
 	}
 
-	public void submit(View v)
+	public void submit(View v) throws NoSuchAlgorithmException, UnsupportedEncodingException
 	{
 		final SharedPreferences app_preferences = PreferenceManager.getDefaultSharedPreferences(this);
 		final SharedPreferences.Editor editor = app_preferences.edit();
@@ -50,28 +52,28 @@ public class sessionNew extends Activity {
 			editor.commit();
 		}
 
-		EditText editText_sessionName, editText_sessionID;
+		EditText editText_sessionName, editText_sessionID, editText_sessionPassword;
 
 		editText_sessionName = (EditText)findViewById(R.id.editText_sessionName);
 		editText_sessionID = (EditText)findViewById(R.id.editText_sessionID);
-
+		editText_sessionPassword = (EditText) findViewById(R.id.editText_sessionPassword);
+		
 		fname = app_preferences.getString("fname", "");
 		lname = app_preferences.getString("lname", "");
 		sessName = editText_sessionName.getText().toString().replace(" ", "%20");
+		sessionPassword = serveUtilities.SHA1(editText_sessionPassword.getText().toString());
 		sID = editText_sessionID.getText().toString();
 		
 		editor.putString("sessionID", sID);
 		editor.commit();
 
-		String url = String.format("http://www.fsurugby.org/serve/request.php?new_session=1&sessionID=%s&sessionName=%s&fname=%s&lname=%s", sID, sessName, fname, lname);
+		String url = String.format("http://www.fsurugby.org/serve/request.php?new_session=1&sessionID=%s&sessionName=%s&fname=%s&lname=%s&password=%s", sID, sessName, fname, lname, sessionPassword);
 		String result = "";
 		try {
 			result = serveUtilities.getStringFromUrl(url);
 		} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
